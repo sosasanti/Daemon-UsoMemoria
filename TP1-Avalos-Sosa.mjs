45f58976a97f57320a2f06fd9f969f258f93ad3a
Santi Sosa <santiago.sosaa2002@gmail.com>
2024-04-20 15:28:14 -0300

    falta definir ejecucion

--- TP1-Avalos-Sosa.mjs
+++ TP1-Avalos-Sosa.mjs
@@ -10,18 +10,14 @@ const emailAddress = process.env.EMAIL_ADDRESS;
 const emailPassword = process.env.EMAIL_PASSWORD;
 
 
-
-
-console.log("email ",emailAddress);
-
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     host:"smtp.gmail.com",
     port:587,
     secure:false,
     auth: {
-        user: emailAddress, // Accede al correo electrónico desde la variable de entorno
-        pass: emailPassword // Accede a la contraseña desde la variable de entorno
+        user: emailAddress,
+        pass: emailPassword 
     }
 });
 
@@ -33,18 +29,13 @@ let mailOptions = {
     attachments: [
         {
             filename: 'reporte.txt',
-            content: 'Contenido del archivo adjunto'
+            path:'reporte.txt'
         }
     ]
 };
 
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.error('Error al enviar el correo electrónico:', error);
-    } else {
-        console.log('Correo electrónico enviado:', info.response);
-    }
-});
+
+
 
 // Funcion lectura arch de configuracion
 function readConfigFile() {
@@ -86,7 +77,6 @@ const PorcentajeMinimoMemoria= config.PorcentajeMinimoMemoria;
 const totalMemoryBytes = os.totalmem();
 const totalMemoryKB = totalMemoryBytes / 1024;
 console.log('Memoria total en KB:', totalMemoryKB, 'KB');
-console.log("Memoeria total en MB: ", totalMemoryKB/1024, "MB"); 
 console.log("memoria total en GB: ", totalMemoryKB/1024/1024, "GB");   
 console.log("cant minima de memoria asignada para que sea reconocido: ", totalMemoryKB * PorcentajeLimiteMemoria/100, "KB"); // 1% de la memoria total del sistema
 
@@ -108,14 +98,17 @@ function getProcessInfo(processes, index,totalMemoryKB) {
                 
                 case "Memoria":
                     const memoryInKB = stats.memory / 1024;
-                    if (memoryInKB > totalMemoryKB * PorcentajeLimiteMemoria/100 && processes[index].cpu !==0) { //Envia mail automaticamente
+                    if (memoryInKB > totalMemoryKB * PorcentajeLimiteMemoria/100) { //Envia mail automaticamente
                             console.log(`Información del proceso con +${PorcentajeLimiteMemoria}% de memoria:`);
-                            console.log(`PID: ${process.pid}`);
-                            console.log(`Nombre: ${process.name}`);
-                            console.log(`Memoria: ${memoryInKB} KB`);
-                            console.log(`CPU: ${processes[index].cpu}`);
+                            console.log(`PID: ${process.pid}`);console.log(`Nombre: ${process.name}`);console.log(`Memoria: ${memoryInKB} KB`);
+                            const logMessage = `Información del proceso con uso de Memoria critica:\n` +
+                            `PID: ${process.pid}\n` +
+                            `Nombre: ${process.name}\n` +
+                            `Memoria: ${memoryInKB} KB\n` +
+                            `CPU: ${processes[index].cpu}\n\n`;
                             console.log("\n");
-                            //enviar correo
+                            writeToReportFile(fs, logMessage);
+                            
                     }
                     else if (memoryInKB > totalMemoryKB * PorcentajeMinimoMemoria/100 && memoryInKB < totalMemoryKB * PorcentajeLimiteMemoria/100){
                         console.log(`Información del procesos LIMITE INTERMEDIO de memoria:`);
@@ -125,18 +118,26 @@ function getProcessInfo(processes, index,totalMemoryKB) {
                         console.log(`CPU: ${processes[index].cpu}`);
                         console.log("\n");
                         //Logeo de la info en el archivo
-                        const logMessage = `Información del procesos LIMITE INTERMEDIO de memoria:\n` +
+                        const logMessage = `Información del proceso:\n` +
                         `PID: ${process.pid}\n` +
                         `Nombre: ${process.name}\n` +
                         `Memoria: ${memoryInKB} KB\n` +
                         `CPU: ${processes[index].cpu}\n\n`;
                 
-                    writeToReportFile(fs, logMessage);
+                   
                     }
                     break;
                 case "CPU":
-                    console.log("se esta monitoreando el uso de CPU");
-                
+                    const usoCPU = processes[index].cpu;
+                    if (usoCPU > PorcentajeLimiteCPU){
+                        const logMessage = `Información del proceso con consumo de CPU critico:\n` +
+                        `PID: ${process.pid}\n` +
+                        `Nombre: ${process.name}\n` +
+                        `Memoria: ${memoryInKB} KB\n` +
+                        `CPU: ${processes[index].cpu}\n\n`;
+                        writeToReportFile(fs, logMessage);
+                    }
+
                     break;
                 default:
                     console.log("opcion incorrecta");
@@ -150,7 +151,7 @@ function getProcessInfo(processes, index,totalMemoryKB) {
 
 
 
-// OBTIENE LA LISTA DE PROCESOS, Y LUEGO OBTIENE LA INFORMACIÓN DE CADA PROCESO
+// Obtiene la lista de procesos, luego la info de cada uno
 psList().then(processes => {
 
     getProcessInfo(processes, 0,totalMemoryKB);
@@ -161,3 +162,10 @@ psList().then(processes => {
 
 
 
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.error('Error al enviar el correo electrónico:', error);
+    } else {
+        console.log('Correo electrónico enviado:', info.response);
+    }
+});
