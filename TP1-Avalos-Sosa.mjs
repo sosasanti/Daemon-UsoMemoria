b2bccfe72f7f172211ab70b61daa1dd021625ee7
Wencho8 <wenchoavalos8@gmail.com>
2024-04-24 12:06:59 -0300

    Daemon linked to functionality.

--- TP1-Avalos-Sosa.mjs
+++ TP1-Avalos-Sosa.mjs
@@ -4,96 +4,168 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 const configFile = 'config.json';
 const reportFile = 'reporte.txt';
+import path from 'path';
+import { spawn } from 'child_process';
 
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+const piddir = '/var/run/daemond/';
+const pidfile = 'daemondP.pid';
+const pidfullpath = path.join(piddir, pidfile);
+
+const logdir = '/var/log/daemond/';
+const logfile = 'daemondLogStatus.log';
+const logfullpath = path.join(logdir, logfile);
+
+dotenv.config();
+let emailAddress = process.env.EMAIL_ADDRESS;
+let emailPassword = process.env.EMAIL_PASSWORD;
+let transporter;
+
+
+function createPidFile(fullpath) {
+    fs.promises.writeFile(fullpath, process.pid.toString())
+        .catch(err => {
+            throw err;
+        });
+}
+
+//Funcion para detener el daemon con el comando "stop", desde fuera del daemon.
+function stopDaemon() {
+    if (fs.existsSync(pidfullpath)) {
+        const pid = parseInt(fs.readFileSync(pidfullpath, 'utf8'));
+        if (isNaN(pid)) {
+            console.error('Invalid PID');
+            return;
+        }
+        try {
+            process.kill(pid);
+            console.log('Daemon stopped successfully');
+        } catch (error) {
+            console.error('Error stopping the daemon:', error.message);
+        }
+    } else {
+        console.log('Daemon is not running');
+    }
+}
 
 // Funcion lectura arch de configuracion
 function readConfigFile() {
+    const configFile = path.join(__dirname, 'config.json');
     try {
         const rawConfig = fs.readFileSync(configFile);
         const config = JSON.parse(rawConfig);
         return config;
     } catch (error) {
-        console.error(`Error al leer el archivo de configuración: ${error.message}`);
+        const errorMessage = `Error al leer el archivo de configuración: ${error.message}\n`;
+        fs.appendFile(logfullpath, errorMessage, function (err) {
+            if (err) throw err;
+        });
         return null;
     }
 }
 
 // Escribe el archivo de reporte
 function writeToReportFile(data) {
+    const reportFile = path.join(__dirname, 'reporte.txt');
     try {
         fs.appendFileSync(reportFile, data + '\n');
     } catch (err) {
-        console.error('Error al escribir en el archivo de reporte:', err);
+        const errorMessage = `Error al escribir en el archivo de reporte: ${err.message}\n`;
+        fs.appendFile(logfullpath, errorMessage, function (err) {
+            if (err) throw err;
+        });
     }
 }
 
 function borrarContenidoArchivo(archivo) {
-    fs.writeFile(archivo, '', (err) => {
+    const filePath = path.join(__dirname, archivo);
+    fs.writeFile(filePath, '', (err) => {
         if (err) {
-            console.error('Error al borrar el contenido del archivo:', err);
+            const errorMessage = `Error al borrar el contenido del archivo ${archivo}: ${err.message}\n`;
+            fs.appendFile(logfullpath, errorMessage, function (err) {
+                if (err) throw err;
+            });
         } else {
-            console.log('Contenido del archivo borrado correctamente.');
+            const message = `Contenido del archivo ${archivo} borrado correctamente.`
+            fs.appendFile(logfullpath, message, function (err) {
+                if (err) throw err;
+            });
         }
     });
 }
 
 //Analiza el consumo de recursos por proceso
-function monitorearRecurso(proceso, config){
-
-    const {tipoMonitoreo, PorcentajeLimiteCPU, PorcentajeMinimoCPU, PorcentajeLimiteMemoria, PorcentajeMinimoMemoria } = config;
+function monitorearRecurso(proceso, config) {
+    const { tipoMonitoreo, PorcentajeLimiteCPU, PorcentajeMinimoCPU, PorcentajeLimiteMemoria, PorcentajeMinimoMemoria } = config;
     const esCPU = tipoMonitoreo === "CPU";
     const esMemoria = tipoMonitoreo === "Memoria";
 
     if (esCPU && proceso.cpu > PorcentajeLimiteCPU || esMemoria && proceso.memory > PorcentajeLimiteMemoria) {
-        console.log("nombre ",proceso.name," CPU: ",proceso.cpu," Memoria: ",proceso.memory); //sacar dsp
         const asunto = esCPU ? "Consumo de CPU crítico" : "Consumo de Memoria crítico";
         const logMessage = `Información del proceso con uso de ${tipoMonitoreo} crítico:\n` +
             `PID: ${proceso.pid}\n` +
             `Nombre: ${proceso.name}\n` +
-            `Memoria: ${proceso.memory} \n` +
-            `CPU: ${proceso.cpu} \n\n`;
+            `Memoria: ${proceso.memory} % \n` +
+            `CPU: ${proceso.cpu} % \n\n`;
         enviarCorreo(asunto, logMessage);
-    } 
-    else if (esCPU && proceso.cpu > PorcentajeMinimoCPU || esMemoria && proceso.memory > PorcentajeMinimoMemoria) {
+    }
+    if (esCPU && proceso.cpu > PorcentajeMinimoCPU || esMemoria && proceso.memory > PorcentajeMinimoMemoria) {
         const logMessage = `Información del proceso con uso de ${tipoMonitoreo} crítico:\n` +
             `PID: ${proceso.pid}\n` +
             `Nombre: ${proceso.name}\n` +
-            `Memoria: ${proceso.memory} \n` +
-            `CPU: ${proceso.cpu} \n\n`;
+            `Memoria: ${proceso.memory} % \n` +
+            `CPU: ${proceso.cpu} % \n\n`;
         writeToReportFile(logMessage);
     }
 }
 
 
 //Recorre lista de procesos
-async function MonitoreoRecursos(config){
-    try{
+async function MonitoreoRecursos(config) {
+    try {
         const procesos = await psList();
 
         for (let i = 0; i < procesos.length; i++) {
             const proceso = procesos[i];
             try {
-                monitorearRecurso(proceso,config);
-                    
+                monitorearRecurso(proceso, config);
+
             } catch (error) {
-                console.error('Error al obtener información del proceso:', error);
+                fs.appendFile(logfullpath, 'Error al obtener información del proceso', function (err) {
+                    if (err) throw err;
+                });
             }
         }
     }
-    catch(error){
-        console.error('Error al obtener la lista de procesos:', error);
+    catch (error) {
+        fs.appendFile(logfullpath, 'Error al obtener la lista de procesos:', function (err) {
+            if (err) throw err;
+        });
     }
-    finally{
+    finally {
         envioReporteDiario();
     }
 }
 
 // Enviar archivo actual de reporte
-function envioReporteDiario() { 
-    enviarCorreo('Reporte diario de procesos con recursos elevados', 'Reporte adjunto','reporte.txt',borrarContenidoArchivo);
+function envioReporteDiario() {
+    enviarCorreo('Reporte diario de procesos con recursos elevados', 'Reporte adjunto', 'reporte.txt', borrarContenidoArchivo);
 }
 
-function enviarCorreo(asunto, texto,adjuntoPath = null,callback) {
+function enviarCorreo(asunto, texto, adjuntoPath = null, callback) {
+    transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: emailAddress,
+            pass: emailPassword
+        }
+    });
+
     let mailOptions = {
         from: emailAddress,
         to: 'santiago.sosaa2002@gmail.com',
@@ -111,6 +183,10 @@ function enviarCorreo(asunto, texto,adjuntoPath = null,callback) {
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.error('Error al enviar el correo electrónico:', error);
+            const errorMessage = `Error al enviar el correo electrónico: ${error.message}\n`;
+            fs.appendFile(logfullpath, errorMessage, function (err) {
+                if (err) throw err;
+            });
         } else {
             console.log('Correo electrónico enviado:', info.response);
             if (callback) {
@@ -120,39 +196,59 @@ function enviarCorreo(asunto, texto,adjuntoPath = null,callback) {
     });
 }
 
-//Variables scope global
-dotenv.config();
-const emailAddress = process.env.EMAIL_ADDRESS;
-const emailPassword = process.env.EMAIL_PASSWORD;
+if (process.argv[2] === 'stop') {
+    stopDaemon();
+}
+else if (process.argv[2] !== 'daemon') {  //El padre
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host:"smtp.gmail.com",
-    port:587,
-    secure:false,
-    auth: {
-        user: emailAddress,
-        pass: emailPassword 
+    if (!fs.existsSync(piddir)) {       //Si no existe el directorio lo crea.
+        fs.mkdirSync(piddir, { recursive: true });
     }
-});
+    if (fs.existsSync(pidfullpath)) {   //Si existe, el daemon ya arrancó
+        console.log('Daemon is already running');
+        process.exit();
+    }
 
-//logica principal 
-function main(){ 
+    const child = spawn(process.argv[0], [__filename, 'daemon'], {  //Se hace el fork y se desreferencia (Puede usarse .fork tambien)
+        cwd: '/',
+        detached: true,
+        stdio: 'ignore'
+    });
+    console.log(child.pid)
+    child.unref();
+    process.exit();  //El padre se elimina
+} else {
+    function onKill() {         //Se agregan eventos de destruccion desde el daemon.
+        fs.unlinkSync(pidfullpath);
+        process.exit();
+    }
+    process.on('SIGTERM', onKill);
+    process.on('SIGHUP', onKill);
 
+    process.umask(0o022);
+    createPidFile(pidfullpath); //Se escribe el id en el archivo .pid
+    
+    if (!fs.existsSync(logdir)) {
+        fs.mkdirSync(logdir, { recursive: true });
+    } 
+
+    main();
+}
+
+ 
+function main() {
     const config = readConfigFile();
     if (config === null) {
-        console.log('No se pudo cargar el archivo de configuración');
+        fs.appendFile(logfullpath, 'No se pudo cargar el archivo de configuración', function (err) {
+            if (err) throw err;
+        });
     }
-    else{
-        
-        MonitoreoRecursos(config); 
+    else {
+
+        MonitoreoRecursos(config);
         setInterval(() => MonitoreoRecursos(config), 60000);
 
         setInterval(envioReporteDiario, 120000); //cada 2min ahora
     }
 
 }
-
-main();
-
-
